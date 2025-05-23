5d8c7e6f027f7dadf0eb7fd8a3486ad1aa3131db
ChrisFSotelo <sotelolancheroscristianferney@gmail.com>
2025-05-23 11:09:52 -0500

    HU-01: consultas de base de datos.

---
+++ src/feactures/users/dao/ClienteDAO.php
@@ -0,0 +1,95 @@
+<?php
+
+
+namespace dao;
+
+use Conexion;
+use models\Clientes;
+
+class ClienteDAO{
+    private $conexion;
+
+    public function __construct()
+    {
+        $this->conexion = new Conexion();
+    }
+
+    //insertar
+    public function RegistrarCliente(Clientes $cliente)
+    {
+        $this->conexion->abrirConexion();
+        $nombre = $cliente->getNombre();
+        $apellido = $cliente->getApellido();
+        $correo = $cliente->getCorreo();
+        $clave = $cliente->getClave();
+        $idRol = $cliente->getIdRol();
+
+        $sql = "INSERT INTO  clientes (nombre, apellido, correo, clave, idRol) VALUES ('$nombre','$apellido','$correo','$clave',$idRol)";
+        $resultado = $this->conexion->ejecutarConsulta($sql);
+        $this->conexion->cerrarConexion();
+        if($resultado)
+            return $cliente;
+
+        echo ("Hubo un error al registrar el cliente");
+        return null;
+    }
+
+    public function obtenerPorId($id){
+        $this->conexion->abrirConexion();
+        $sql = "SELECT  nombre, apellido, correo FROM clientes WHERE id = $id";
+        $resultado = $this->conexion->ejecutarConsulta($sql);
+
+        if(!$resultado) { // Si hubo un error
+            $this->conexion->cerrarConexion();
+            echo("Hubo un fallo al obtener el cliente \n");
+            return null;
+        }
+
+        if ($fila = $resultado ->fetch_assoc())
+            $cliente = new Clientes($fila['nombre'], $fila['apellido'], $fila['correo'], $fila['idRol']);
+        $this->conexion->cerrarConexion();
+        return $cliente;
+
+    }
+
+    public function actualizar(Clientes $cliente) {
+        $this->conexion->abrirConexion();
+
+        $id = $cliente->getId();
+        $nombre = $cliente->getNombre();
+        $apellido = $cliente->getApellido();
+        $correo = $cliente->getCorreo();
+        $clave = $cliente->getClave();
+
+        $sql = "UPDATE clientes SET nombre = '$nombre', apellido = '$apellido', correo = '$correo', clave = '$clave'  WHERE id = $id";
+        $resultado = $this->conexion->ejecutarConsulta($sql);
+
+        $this->conexion->cerrarConexion();
+        return $resultado;
+    }
+
+    public function listar() {
+        $this->conexion->abrirConexion();
+
+        $sql = "SELECT nombre, apellido, correo FROM clientes";
+        $resultado = $this->conexion->ejecutarConsulta($sql);
+        $clientes = [];
+
+        while ($fila = $resultado->fetch_assoc())
+            $clientes[] = new Clientes($fila['nombre'], $fila['apellido'], $fila['correo']);
+
+        $this->conexion->cerrarConexion();
+        return $clientes;
+    }
+
+    public function eliminar($id) {
+        $this->conexion->abrirConexion();
+
+        $sql = "DELETE FROM clientes WHERE id = $id";
+        $resultado = $this->conexion->ejecutarConsulta($sql);
+
+        $this->conexion->cerrarConexion();
+        return $resultado;
+    }
+
+}
