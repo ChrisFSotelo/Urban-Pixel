89cf4bc70f1d6714bc18dd3e46a39dd26a616aee
Fabian Caro <fmcaror@udistrital.edu.co>
2025-05-22 19:04:29 -0500

    Creación del archivo routes

---
+++ config/routes.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    '/' => [
+        'view' => 'src/features/users/views/landing_page.php',
+        'allowed_roles' => [],
+        'requires_session' => false,
+    ],
+    '/login' => [
+        [
+            'view' => 'src/features/login/views/login.php',
+        ],
+    ],
+];
+?>
