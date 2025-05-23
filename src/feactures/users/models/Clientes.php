3db100d6d44979fcc68a4024abb54cabd6ce8ec3
ChrisFSotelo <sotelolancheroscristianferney@gmail.com>
2025-05-23 10:51:48 -0500

    HU-01: modelo de datos clientes

---
+++ src/feactures/users/models/Clientes.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace models;
+
+class Clientes
+{
+    private int $id;
+    private string $nombre;
+    private string $apellido;
+    private string $correo;
+    private string $clave;
+    private int $idRol;
+
+
+    public function __construct(int $id, string $nombre, string $apellido, string $correo, string $clave, int $idRol)
+    {
+        $this->id = $id;
+        $this->nombre = $nombre;
+        $this->apellido = $apellido;
+        $this->correo = $correo;
+        $this->clave = $clave;
+        $this->idRol = $idRol;
+    }
+
+    //Getters
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function getApellido()
+    {
+        return $this->apellido;
+    }
+    public function getCorreo(){
+        return $this->correo;
+    }
+    public function getClave(){
+        return $this->clave;
+    }
+    public function getIdRol(){
+        return $this->idRol;
+    }
+
+    //Setters
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    public function setApellido($apellido)
+    {
+        $this->apellido = $apellido;
+    }
+
+}
+
